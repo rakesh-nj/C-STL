67e7c4ff3107e52fbda815581490a60ddec479c4
Rakesh N J <121295004+rakesh-nj@users.noreply.github.com>
2023-02-22 22:19:25 +0530

    Update Pairs .cpp

--- Pairs .cpp
+++ Pairs .cpp
@@ -1,1 +1,12 @@
-
+void explain_pair() {
+  
+  #declaring pair
+  pair<int ,int >p1;
+  p1 = {1,2};
+  pair<int,char>p2;
+  p2 = {3,'c'};
+  
+  #printing pair
+  cout<<"p1 : "<<p1.first<<" "<<p1.second<<endl;
+  cout<<"p2 : "<<p2.first<<" "<<p2.second<<endl;
+}
